7eb03d0dc7a8df29f279e3ac360c32c323998593
matheuslimabranco <47332289+matheuslimabranco@users.noreply.github.com>
2021-10-27 18:53:13 -0300

    Create Criar Extensões.sql

---
+++ DDL_Geral_aFarma/Criar Extensões.sql
@@ -0,0 +1,15 @@
+-- Extension: uuid-ossp
+
+-- DROP EXTENSION uuid-ossp;
+
+CREATE EXTENSION uuid-ossp;
+	SCHEMA "public"
+	VERSION 1.1;
+
+
+
+CREATE EXTENSION dblink;
+
+CREATE extension unaccent ;
+
+CREATE extension pg_trgm ;
