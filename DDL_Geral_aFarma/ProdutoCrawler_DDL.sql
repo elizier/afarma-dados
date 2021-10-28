a86bf37703aa4c1d02d899333e292888e9bf49f5
matheuslimabranco <47332289+matheuslimabranco@users.noreply.github.com>
2021-10-28 18:22:01 -0300

    Update ProdutoCrawler_DDL.sql

--- DDL_Geral_aFarma/ProdutoCrawler_DDL.sql
+++ DDL_Geral_aFarma/ProdutoCrawler_DDL.sql
@@ -1,4 +1,3 @@
-
 -- afarma.produtocrawler definition
 
 -- Drop table
@@ -7,18 +6,24 @@
 
 CREATE TABLE afarma.produtocrawler (
 	id varchar(36) NOT NULL,
-	contraindicacao varchar(255) NULL,
-	descricao varchar(255) NULL,
-	ean varchar(255) NULL,
-	indicacao varchar(255) NULL,
-	nome varchar(255) NULL,
+	contraindicacao varchar(10240) NULL,
+	descricao varchar(10240) NULL,
+	ean varchar(10240) NULL,
+	indicacao varchar(10240) NULL,
+	nome varchar(10240) NULL,
 	categoria_id varchar(36) NULL,
 	departamento_id varchar(36) NOT NULL,
 	marca_id varchar(36) NULL,
 	photo_id varchar(36) NULL,
-	CONSTRAINT produtocrawler_pkey PRIMARY KEY (id),
-	CONSTRAINT fkamfmr7j1eatbljs7s093m0mtt FOREIGN KEY (photo_id) REFERENCES afarma.photo(id),
-	CONSTRAINT fkb7mikdij11onlogwu120ht5kh FOREIGN KEY (departamento_id) REFERENCES afarma.departamento(id),
-	CONSTRAINT fkojfoi6s0nxhrqc79m511ax0mu FOREIGN KEY (marca_id) REFERENCES afarma.marca(id),
-	CONSTRAINT fksu0q8dnhupn9aqum3yd3n2c8p FOREIGN KEY (categoria_id) REFERENCES afarma.categoria(id)
+	principioativo_id varchar(36) NULL,
+	produto_tsv tsvector NULL,
+	CONSTRAINT produtocrawler_pkey PRIMARY KEY (id)
 );
+
+
+-- afarma.produtocrawler foreign keys
+
+ALTER TABLE afarma.produtocrawler ADD CONSTRAINT fkamfmr7j1eatbljs7s093m0mtt FOREIGN KEY (photo_id) REFERENCES afarma.photo(id);
+ALTER TABLE afarma.produtocrawler ADD CONSTRAINT fkb7mikdij11onlogwu120ht5kh FOREIGN KEY (departamento_id) REFERENCES afarma.departamento(id);
+ALTER TABLE afarma.produtocrawler ADD CONSTRAINT fkojfoi6s0nxhrqc79m511ax0mu FOREIGN KEY (marca_id) REFERENCES afarma.marca(id);
+ALTER TABLE afarma.produtocrawler ADD CONSTRAINT fksu0q8dnhupn9aqum3yd3n2c8p FOREIGN KEY (categoria_id) REFERENCES afarma.categoria(id);
