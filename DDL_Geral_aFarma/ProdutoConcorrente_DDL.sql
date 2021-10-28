fe3d6941ea4afd908356759a8c1c301471779df4
matheuslimabranco <47332289+matheuslimabranco@users.noreply.github.com>
2021-10-28 18:21:59 -0300

    Update ProdutoConcorrente_DDL.sql

--- DDL_Geral_aFarma/ProdutoConcorrente_DDL.sql
+++ DDL_Geral_aFarma/ProdutoConcorrente_DDL.sql
@@ -1,4 +1,3 @@
-
 -- afarma.produtoconcorrente definition
 
 -- Drop table
@@ -9,11 +8,10 @@ CREATE TABLE afarma.produtoconcorrente (
 	id varchar(36) NOT NULL,
 	dataatualizacao timestamp NULL,
 	ean varchar(255) NULL,
-	url varchar(255) NULL,
+	url varchar(10240) NULL,
 	valor float8 NULL,
 	concorrente_id varchar(36) NOT NULL,
 	produto_id varchar(36) NOT NULL,
-	CONSTRAINT produtoconcorrente_pkey PRIMARY KEY (id),
-	CONSTRAINT fk5oqv18t6pu8wl7v8pwb7n87cw FOREIGN KEY (produto_id) REFERENCES afarma.produtocrawler(id),
-	CONSTRAINT fkrufiectuu75gs63ktxgx3dn34 FOREIGN KEY (concorrente_id) REFERENCES afarma.concorrente(id)
+	concorrente varchar(255) NULL,
+	CONSTRAINT produtoconcorrente_pkey PRIMARY KEY (id)
 );
