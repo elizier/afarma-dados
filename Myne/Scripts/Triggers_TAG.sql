98fc6755be23f6950d0ec7d9d58b8583a7d65b5c
matheuslimabranco <47332289+matheuslimabranco@users.noreply.github.com>
2021-10-27 21:46:10 -0300

    Update Triggers_TAG.sql

--- Myne/Scripts/Triggers_TAG.sql
+++ Myne/Scripts/Triggers_TAG.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION public.taginsert()
+CREATE OR REPLACE FUNCTION public.taguserinsert()
  RETURNS trigger
  LANGUAGE plpgsql
 AS $function$
@@ -28,9 +28,46 @@ $function$
 ;
 
 
-
-create trigger inserttag after
+create trigger insertusertag after
 insert
     on
-    public.myneuser for each row execute function taginsert()
+    public.myneuser for each row execute function taguserinsert()
 
+
+
+
+CREATE OR REPLACE FUNCTION public.tagproductinsert()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+
+BEGIN
+
+
+insert into tag(tag, tag_tsv) select a.tag1, a.to_tsvector from
+(select concat(p.name,' ', p.producttype)  as tag1 , to_tsvector('portuguese', unaccent(concat(p.name,' ', p.producttype)))
+from myneresourceinformation m, product p
+where replace(m.mri, 'mri::', '') = p.id
+except
+select t.tag, t.tag_tsv from tag t) a;
+
+insert into resourcetag(resource, tag) select a.id, a.tag_id from
+(select m.id, t.id as tag_id 
+from myneresourceinformation m, product p, tag t 
+where replace(m.mri, 'mri::', '') = p.id and concat(p.name,' ', p.producttype) = t.tag
+except
+select r.resource, r.tag from resourcetag r) a;
+
+RETURN NEW;
+
+END;
+
+$function$
+;
+
+
+
+create trigger insertproducttag after
+insert
+    on
+    public.product for each row execute function taguserinsert()
