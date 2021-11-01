20b5f2f047cfa012a866a234d9c173d5f00ed061
matheuslimabranco <47332289+matheuslimabranco@users.noreply.github.com>
2021-11-01 15:08:17 -0300

    Create Cotação_Produto_Afarma.sql

---
+++ aFarma/Scripts/Cotação_Produto_Afarma.sql
@@ -0,0 +1,28 @@
+
+
+
+-- DROP TABLE afarma.registrocotacao_afarma;
+
+CREATE TABLE afarma.registrocotacao_afarma (
+	id varchar(36) NOT null default uuid_generate_v4(),
+	"data" timestamp NULL,
+	email varchar(255) NULL,
+	nome varchar(255) NULL,
+	status varchar(255) NULL,
+	uf varchar(255) NULL,
+	CONSTRAINT registrocotacao_pkey_afarma PRIMARY KEY (id)
+);
+
+
+-- Drop table
+
+-- DROP TABLE afarma.itenscot_afarma;
+
+CREATE TABLE afarma.itenscot_afarma (
+	id varchar(36) NOT null default uuid_generate_v4(),
+	cotacao varchar(255) NULL,
+	id_produto varchar(36) NULL,
+	quantidade int4 NOT NULL,
+	CONSTRAINT itenscot_afarma_pkey PRIMARY KEY (id),
+	CONSTRAINT itenscot_afarma_fkey FOREIGN KEY (cotacao) REFERENCES afarma.registrocotacao_afarma(id)
+);
