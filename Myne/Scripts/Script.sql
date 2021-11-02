b8f8abade0f40ab25aa594f5191eb7d4e36d2843
matheuslimabranco <47332289+matheuslimabranco@users.noreply.github.com>
2021-11-02 19:22:25 -0300

    Create Script.sql

---
+++ Myne/Scripts/Script.sql
@@ -0,0 +1,4 @@
+select findresourcedata('304bd32d-afd6-46c6-a2e6-49d74b86aa24')
+
+update public.s3file set s3url = 'https://images-na.ssl-images-amazon.com/images/I/81pXbTDkkCL.jpg'
+where id = 'c617d215-2b04-4854-b5cc-5072a9e64ddd'
