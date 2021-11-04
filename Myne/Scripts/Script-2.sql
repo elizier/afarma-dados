f61851dcd6a9675e29904558a46d0ef8d1863320
matheuslimabranco <47332289+matheuslimabranco@users.noreply.github.com>
2021-11-04 18:04:15 -0300

    Update Script-2.sql

--- Myne/Scripts/Script-2.sql
+++ Myne/Scripts/Script-2.sql
@@ -132,4 +132,76 @@ union all
 ) i)
 
 
-				
+
+
+
+
+
+
+select cast(uuid_generate_v4() as varchar) as  id, 'POST' as type, u.data from
+(
+select jsonb_build_object('user', u.user_data) || u.post as data  from 
+(
+select u.user_data, u.post_data || jsonb_build_object('nested', array_agg(u.data))  as post from
+(
+select  jsonb(u.user_data) as user_data, jsonb(u.post_data) as post_data, jsonb_build_object('type', p.type) || jsonb(p.data) as data from
+(SELECT json_build_object('user',u.user ,'profile_image',u.profile_image)  as user_data, jsonb_build_object('type', p.type) || jsonb(p.data) as post_data, cast(p.data ->> 'createdate' AS TIMESTAMP) as createdate_post, cast(p.data ->> 'id' AS varchar) as id_post
+FROM (
+	select o.post_id, u.id, row_to_json(u.*) AS user
+		,(jsonb_build_object('id', o.s3_id) || jsonb_build_object('createDate', o."createDate") || jsonb_build_object('description', o.description) || jsonb_build_object('fileName', o."fileName") || jsonb_build_object('fileType', o."fileType") || jsonb_build_object('s3url', o.s3url)) AS profile_image
+	FROM (
+		SELECT u.id
+			,u.accountname as "accountName"
+			,u.active
+			,u.createdate as "createDate"
+			,u.devicetoken
+			,u.email
+			,u.name
+			,u.slug
+			,u.usertype
+			,u.visibility
+		FROM myneuser u
+		) u
+		,(
+			SELECT max(s.id) AS s3_id
+				,max(s.createdate) AS "createDate"
+				,max(s.description) AS description
+				,max(s.filename)  as "fileName"
+				,max(s.filetype) as "fileType"
+				,max(s.s3url) AS s3url
+				,o.user_id, o.post_id
+			FROM (
+				SELECT pi.user_id
+					,replace(m.mri, 'mri::', '') AS s3_id, pi.post_id
+				FROM (
+					SELECT u.user_id
+						,o.slave AS id, u.post_id
+					FROM (select m.id as mri_id, replace(m.mri, 'mri::', '') as user_id, a.post_id from
+(select a.owner, replace(m.mri, 'mri::', '') as post_id, a.accountability_id from  
+(select o.owner, a.accountability_id from myneresourceinformation m,
+(select a.id as accountability_id from accountability a
+order by "views" desc, id desc
+limit coalesce(:itens_by_page, 5)
+offset coalesce(:page, 0) * coalesce(:itens_by_page, 5)) a, ownerresources o
+where replace(m.mri, 'mri::', '') = a.accountability_id and o.slave = m.id
+and o.type = 'POST_ACCOUNTABILITY') a, myneresourceinformation m
+where a.owner = m.id ) a, ownerresources o,  myneresourceinformation m
+where a.owner = o.slave and o.owner = m.id) u
+					LEFT JOIN ownerresources o ON u.mri_id = o.OWNER
+						AND o.type = 'USER_PROFILE_IMAGE'
+					) pi
+				LEFT JOIN myneresourceinformation m ON pi.id = m.id
+				) o
+			LEFT JOIN s3file s ON o.s3_id = s.id
+			GROUP BY o.user_id, o.post_id
+			) o
+	WHERE o.user_id = u.id
+	) u
+cross join lateral findresourcedata(u.post_id) as p
+) u
+cross join lateral findresourcebyowner(u.id_post) as p) u
+group by u.user_data, u.post_data) u
+) u
+
+
+select now()
