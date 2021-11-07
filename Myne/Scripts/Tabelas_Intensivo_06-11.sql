64e1a997a48d34065b5344a95845de75c8cc7741
matheuslimabranco <47332289+matheuslimabranco@users.noreply.github.com>
2021-11-07 16:19:10 -0300

    Update Tabelas_Intensivo_06-11.sql

--- Myne/Scripts/Tabelas_Intensivo_06-11.sql
+++ Myne/Scripts/Tabelas_Intensivo_06-11.sql
@@ -7,14 +7,15 @@ CREATE TABLE public.price (
 );
 
 CREATE TABLE public.purchase (
+	id varchar NOT NULL DEFAULT uuid_generate_v4(),
 	launch_id varchar(36) NULL,
 	product_id varchar(36) NULL,
-	user_id varchar(36) NOT NULL,
-	id varchar NOT NULL DEFAULT uuid_generate_v4(),
 	createdate timestamptz NOT NULL DEFAULT now(),
+	value float NOT NULL
 	CONSTRAINT purchase_pk PRIMARY KEY (id),
 	CONSTRAINT purchase_fk FOREIGN KEY (user_id) REFERENCES public.myneuser(id)
 );
+
 
 
 CREATE TABLE public.counterpart (
@@ -23,30 +24,27 @@ CREATE TABLE public.counterpart (
 	description varchar(10240) null
 	);
 
-
-SELECT public.findmyproducts(:user_id, :type_)
-
-select 
-
-update myneuser set slug=o.slugify from
-(
-select o.id as id1, slugify(concat(o.name, ' ',
-(select (max((cast(substring(u.slug FROM '[0-9]+') as numeric))+1)) from public.myneuser u
-where u.slug notnull))) from
-(
-select o.id, slugify(o.name) as name,  ROW_NUMBER() OVER(ORDER BY o.id) AS RowNumber
-from public.myneuser o  where o.slug isnull
-) o) o
-where o.id1=id;
-
-
-update myneuser set accountname = translate(slug,'-','') from
-(select id as id1 from myneuser where accountname isnull) a
-where a.id1=id;
+CREATE TABLE public.purchase_counterpart(
+	purchase_id varchar(36) NOT null,
+	counterpart_id varchar(36) NOT NULL
+	);
 
 
 
+CREATE TABLE public.counterpart (
+	id varchar(36) NOT null,
+	TYPE varchar(10240) null,
+	description varchar(10240) null
+	);
 
+USER_PURCHASE
+
+
+
+CREATE INDEX resource_index ON public.myneresourceinformation (id, mri);
+
+
+select findmyneglobalfeed(10,0)
 
 
 
