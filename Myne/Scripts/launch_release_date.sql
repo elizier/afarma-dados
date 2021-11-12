eeacc325b89543851520f2ab549983e3eefb6c48
matheuslimabranco <47332289+matheuslimabranco@users.noreply.github.com>
2021-11-11 23:03:15 -0300

    Update launch_release_date.sql

--- Myne/Scripts/launch_release_date.sql
+++ Myne/Scripts/launch_release_date.sql
@@ -34,6 +34,43 @@ ALTER TABLE public.insight ALTER COLUMN releasedate SET NOT NULL;
 
 ALTER TABLE public.insight ALTER COLUMN releasedate SET DEFAULT now();
 
+-- Launch json
+
+select
+	f.data as launch_data,
+	ow.data as user_data,
+	ro.data as nested,
+	rf.data as profile_image
+from
+	(
+	select
+		m.id
+	from
+		myneresourceinformation m
+	where
+		replace(m.mri, 'mri::', '') = coalesce(:user_id, replace(m.mri, 'mri::', ''))) m
+left join ownerresources o on
+	m.id = o.owner
+left join myneresourceinformation mr on
+	o.slave = mr.id
+left join lateral findresourcedata(replace(mr.mri, 'mri::', '')) as f on
+	true
+left join lateral findresourcedata(f.owner) as ow on
+	true
+left join lateral findresourcebyowner(cast(f.data ->> 'id' as varchar)) as ro on
+	true
+left join lateral findresourcebyownerandtype(cast(ow.data ->> 'id' as varchar),
+	'PROFILE_IMAGE') as rf on
+	true
+where
+	mr.type = 'LAUNCH'
+
+
+
+select public.myneresearch('matheus', 'USER', 10,0)
+
+
+
 
 
 
