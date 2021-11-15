607faab9dbdb113930bb88b12fea3ad9cfdf83d0
matheuslimabranco <47332289+matheuslimabranco@users.noreply.github.com>
2021-11-15 14:30:40 -0300

    Update Global research.sql

--- Myne/Scripts/Global research.sql
+++ Myne/Scripts/Global research.sql
@@ -446,3 +446,246 @@ $function$
 
 update global.research set ts_vector = 'USER' where "type" isnull
 select  * from global.research r 
+
+
+
+CREATE OR REPLACE FUNCTION public.checkpurchase(user_id character varying , product character varying)
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$
+begin 
+	
+select (case when p.product_id isnull then false else true end) as purchase from
+(select o.slave, replace(m.mri, 'mri::', '') as user_id  from myneresourceinformation m, ownerresources o 
+where replace(m.mri, 'mri::', '') = user_id and o."owner" = m.id and o."type" = 'USER_PURCHASE') up
+left join
+myneresourceinformation m on m.id = up.slave
+left join purchase p on replace(m.mri, 'mri::', '') = p.id
+where p.product_id = product
+into purchased;
+
+   return purchased;
+
+END;
+
+$function$
+;
+
+select public.checkpurchase(:user_id, :product_id)
+
+
+
+CREATE OR REPLACE FUNCTION public.checkpurchase(user_id character varying , product character varying)
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$
+declare
+   purchased boolean;
+begin
+	
+	select (case when p.product_id isnull then false else true end) as purchase from
+(select o.slave, replace(m.mri, 'mri::', '') as user_id  from myneresourceinformation m, ownerresources o 
+where replace(m.mri, 'mri::', '') = user_id and o."owner" = m.id and o."type" = 'USER_PURCHASE') up
+left join
+myneresourceinformation m on m.id = up.slave
+left join purchase p on replace(m.mri, 'mri::', '') = p.id
+where p.product_id = product
+
+into purchased;
+   
+   return purchased;
+end;
+$function$
+;
+
+
+
+(select m.id from myneresourceinformation m, ownerresources o, purchase p 
+where replace(m.mri, 'mri::', '') = p.id and p.product_id = :product_id and o.slave = m.id)
+
+
+select public.findmylaunchs(:user_id , :type_ )
+
+
+
+
+select
+	cast(uuid_generate_v4() as varchar) as id,
+	'LAUNCH' as "type",
+	l.launch_data || jsonb_build_object('nested', array_agg(l.product_data)) || l.user_data as data
+from
+	(
+	select
+		l.launch_data,
+		l.product_data || jsonb_build_object('nested', array_agg(l.product_nested)) as product_data,
+		jsonb_build_object('user', jsonb_build_object('user', l.user_data) || jsonb_build_object('profile_image', l.profile_image)) as user_data
+	from
+		(
+		select
+			jsonb_build_object('type', f.type) || to_jsonb(f.data) as launch_data,
+			jsonb_build_object('type', ow.type) || to_jsonb(ow.data) as user_data,
+			jsonb_build_object('type', ro.type) || to_jsonb(ro.data) as product_data,
+			jsonb_build_object('type', rf.type) || to_jsonb(rf.data) as profile_image,
+			jsonb_build_object('type', pn.type) || to_jsonb(pn.data) as product_nested
+		from
+			(
+			select
+				m.id
+			from
+				myneresourceinformation m
+			where
+				replace(m.mri, 'mri::', '') = coalesce(:user_id, replace(m.mri, 'mri::', ''))
+				and m.type = 'USER') m
+		left join ownerresources o on
+			m.id = o.owner
+		left join (select * from myneresourceinformation mr where mr.type = 'LAUNCH') mr on
+			o.slave = mr.id
+		left join lateral findresourcedata(replace(mr.mri, 'mri::', '')) as f on
+			true
+		left join lateral findresourcedata(f.owner) as ow on
+			true
+		left join lateral findresourcebyowner(cast(f.data ->> 'id' as varchar)) as ro on
+			true
+		left join lateral findresourcebyownerandtype(cast(ow.data ->> 'id' as varchar),
+			'PROFILE_IMAGE') as rf on
+			true
+		left join lateral findresourcebyowner(cast(ro.data ->> 'id' as varchar)) as pn on
+			true
+		where
+			--mr.type = 'LAUNCH'
+			--and 
+			cast(f.data ->> 'launchType' as varchar) = coalesce(nullif(:type_, 'NULO'), cast(f.data ->> 'launchType' as varchar)) ) l
+	group by
+		launch_data,
+		l.user_data,
+		l.product_data,
+		l.profile_image
+		) l
+group by
+	l.launch_data,
+		l.user_data
+		
+		
+		
+select f.* from	
+(select replace(m.mri, 'mri::', '') as resource_id from myneresourceinformation m) m
+left join lateral findresourcedata(m.resource_id) as f on true
+where f.type = 'LAUNCH'
+		
+		
+
+select m.id from myneresourceinformation m
+			where
+				replace(m.mri, 'mri::', '') = coalesce(:user_id, replace(m.mri, 'mri::', ''))
+				and m.type = 'USER'
+
+				
+select * from product p where now() > p.releasedate cast('2021-11-15 10:35:00.000 -0300' as timestamp with time zone)
+
+
+
+select cast(uuid_generate_v4() as varchar) as id,  cast('RESEARCH' as varchar) as type, to_json( r.data) as data  from 
+(select jsonb_build_object('user', (jsonb(ro.data) || jsonb_build_object('profile_image', r.array_agg))) || r.data_post || r.data_slave as data
+from
+(select r.owner,  array_agg(ro.data), r.data_post, r.data as data_slave from
+(select r.owner, r.data_post, jsonb_build_object('nested', array_agg(r.data_slave)) as data from
+(select rd.owner, jsonb_build_object('type', rd.type) || jsonb(rd.data) as data_post ,
+jsonb_build_object('type', ro.type) || jsonb(ro.data) as data_slave from
+(select replace(m.mri,'mri::','') as resource_id, 
+t.id, tsvector_agg(t.tag_tsv), similarity(lower(unaccent(STRING_AGG(t.tag, ' '))), lower(unaccent(:research)))
+from tag t, myneresourceinformation m, resourcetag r 
+where 
+ m.id = r.resource and r.tag = t.id and 
+t.tag_tsv @@
+to_tsquery('portuguese',(select replace(unaccent(trim(:research)),' ',' | ')))
+and m.type = 'POST'
+group by t.id , m.mri
+order by similarity desc
+limit coalesce(:itens_by_page, 5)
+offset coalesce(:page, 0) * coalesce(:itens_by_page, 5)
+) m
+cross join lateral findresourcedata(m.resource_id) as rd
+cross join lateral findresourcebyowner(m.resource_id) as ro) r 
+group by r.owner, r.data_post) r
+left join lateral findresourcebyowner(r.owner) ro on true
+where ro.type = 'PROFILE_IMAGE' or ro.type isnull
+group by r.owner, r.data_post, r.data) r 
+cross join lateral findresourcedata(r.owner) as ro) r;
+
+
+
+
+
+CREATE OR REPLACE FUNCTION public.tagpostinsert()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+
+BEGIN
+
+
+insert into tag(tag, tag_tsv) select a.tag1, a.to_tsvector from
+(select p.title  as tag1 , to_tsvector('portuguese', unaccent(p.title))
+from myneresourceinformation m, post p
+where replace(m.mri, 'mri::', '') = p.id
+except
+select t.tag, t.tag_tsv from tag t) a;
+
+insert into resourcetag(resource, tag) select a.id, a.tag_id from
+(select m.id, t.id as tag_id 
+from myneresourceinformation m, post p, tag t 
+where replace(m.mri, 'mri::', '') = p.id and p.title = t.tag
+except
+select r.resource, r.tag from resourcetag r) a;
+
+RETURN NEW;
+
+END;
+
+$function$
+;
+
+create trigger insertposttag after
+insert
+    on
+    public.post for each row execute function tagpostinsert()
+
+
+
+
+CREATE OR REPLACE FUNCTION public.checkpurchase(user_id character varying, product character varying)
+ RETURNS SETOF mynejsontype
+ LANGUAGE plpgsql
+AS $function$
+   declare
+      resource_t public.mynejsontype%ROWTYPE;
+
+begin
+
+ 	for resource_t in
+ 
+
+	select cast(uuid_generate_v4() as varchar) as id,
+	'PURCHASE' as type, json_build_object('purchased', (case when p.product_id isnull then 'false' else 'true' end)) as purchase from
+(select o.slave, replace(m.mri, 'mri::', '') as user_id  from myneresourceinformation m, ownerresources o 
+where replace(m.mri, 'mri::', '') = user_id and o."owner" = m.id and o."type" = 'USER_PURCHASE') up
+left join
+myneresourceinformation m on m.id = up.slave
+left join purchase p on replace(m.mri, 'mri::', '') = p.id
+where p.product_id = product
+
+loop
+		return next resource_t;
+end loop;
+
+return;
+end;
+
+$function$
+;
+
+
+
+
+
+
